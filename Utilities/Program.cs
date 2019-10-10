8842af0f3e8a8f448291b26583d47c417f0e00cb
davew2040 <dwinterm@gmail.com>
2019-10-10 18:21:39 -0400

    Initial commit

---
+++ Utilities/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UtilitiesLib;
+
+namespace Utilities
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var timer = new ActionTimer(time => Console.WriteLine(time.TotalSeconds)))
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
---
+++ UtilitiesLib/ActionTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace UtilitiesLib
+{
+    public class ActionTimer : IDisposable
+    {
+        private readonly Stopwatch _timer;
+        private readonly Action<TimeSpan> _timerFunc;
+
+        public ActionTimer(Action<TimeSpan> timerFunc)
+        {
+            _timerFunc = timerFunc;
+            _timer = new Stopwatch();
+            _timer.Start();
+        }
+
+        public void Dispose()
+        {
+            _timerFunc(_timer.Elapsed);
+        }
+    }
+}
